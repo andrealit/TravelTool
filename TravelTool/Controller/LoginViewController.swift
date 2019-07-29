6e0d923e75f7053003cde5c9248348aee8728dc0
Andrea Tongsak <atongsak@gmail.com>
2019-07-29 09:54:31 -0700

    Added login view controller

---
+++ TravelTool/Controller/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  TravelTool
+//
+//  Created by Andrea Tongsak on 7/29/19.
+//  Copyright © 2019 Andrea Tongsak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    // MARK: Outlets
+    
+    
+    // MARK: DidLoad Family
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+}
